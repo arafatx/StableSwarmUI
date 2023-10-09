d1fb2150cfb33ae96e76ec32afc756a432dfe123
Arafat Ali <arafatx@users.noreply.github.com>
2023-10-10 03:58:15 +0800

    Update make-shortcut.bat

--- make-shortcut.bat
+++ make-shortcut.bat
@@ -1,22 +1,28 @@
 @echo off
+setlocal enabledelayedexpansion
 
-:: Check if the default desktop location exists
+:: Attempt to set the DesktopPath using the default location
 if exist "%userprofile%\Desktop\" (
     set DesktopPath=%userprofile%\Desktop
 ) else (
-    :: Fetch the desktop location from the registry
-    for /f "tokens=2* delims=   " %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do set DesktopPath=%%b
-    :: Replace %USERPROFILE% environment variables in the path
-    set DesktopPath=%DesktopPath:%USERPROFILE%=%userprofile%
+    :: If not found, try to fetch the desktop location from the registry
+    for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do (
+        set DesktopPath=%%b
+    )
+    :: Replace potential environment variables in the path
+    set DesktopPath=!DesktopPath:%USERPROFILE%=%userprofile%!
 )
 
-set SHORTCUTPATH="%DesktopPath%\StableSwarmUI.url"
+if not defined DesktopPath (
+    echo Error: Could not determine Desktop path.
+    exit /b
+)
+
+set SHORTCUTPATH="!DesktopPath!\StableSwarmUI.url"
 
 (
 echo [InternetShortcut]
 echo URL="%CD%\launch-windows.bat"
 echo IconFile="%CD%\src\wwwroot\favicon.ico"
 echo IconIndex=0
-) >> "%SHORTCUTPATH%"
-
-pause
+) >> !SHORTCUTPATH!
