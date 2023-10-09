982c03bbc8695ab5aec100a7ec63f1aec1589e6b
Arafat Ali <arafatx@users.noreply.github.com>
2023-10-10 03:48:55 +0800

    Fixed shortcut creation issue
    When Desktop location is on different drive let say drive D, the line %userprofile% always point to drive C. So, we add extra registry check.

--- make-shortcut.bat
+++ make-shortcut.bat
@@ -1,7 +1,22 @@
 @echo off
 
-set SHORTCUTPATH="%userprofile%\Desktop\StableSwarmUI.url"
-echo [InternetShortcut] >> "%SHORTCUTPATH%"
-echo URL="%CD%\launch-windows.bat" >> "%SHORTCUTPATH%"
-echo IconFile="%CD%\src\wwwroot\favicon.ico" >> "%SHORTCUTPATH%"
-echo IconIndex=0 >> "%SHORTCUTPATH%"
+:: Check if the default desktop location exists
+if exist "%userprofile%\Desktop\" (
+    set DesktopPath=%userprofile%\Desktop
+) else (
+    :: Fetch the desktop location from the registry
+    for /f "tokens=2* delims=   " %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do set DesktopPath=%%b
+    :: Replace %USERPROFILE% environment variables in the path
+    set DesktopPath=%DesktopPath:%USERPROFILE%=%userprofile%
+)
+
+set SHORTCUTPATH="%DesktopPath%\StableSwarmUI.url"
+
+(
+echo [InternetShortcut]
+echo URL="%CD%\launch-windows.bat"
+echo IconFile="%CD%\src\wwwroot\favicon.ico"
+echo IconIndex=0
+) >> "%SHORTCUTPATH%"
+
+pause
